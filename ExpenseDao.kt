435c2e6fe6e82b315303bc44e0318ccf88b089ad
ivankw <rpladdict25@gmail.com>
2026-09-06 01:09:22 +0700

    Create ExpenseDao.kt

---
+++ ExpenseDao.kt
@@ -0,0 +1,19 @@
+package com.example.pengeluaran.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExpenseDao {
+    @Query("SELECT * FROM expenses ORDER BY date DESC")
+    fun getAllExpenses(): Flow<List<Expense>>
+
+    @Insert
+    suspend fun insertExpense(expense: Expense)
+
+    @Delete
+    suspend fun deleteExpense(expense: Expense)
+}
